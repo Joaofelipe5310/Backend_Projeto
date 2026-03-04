c8a3365f542d074b85d81e244fe6e2ec33fc7f76
João Felipe <joaofelipegaldinodelima75@gmail.com>
2026-03-04 16:44:14 -0300

    ##saving progress

--- src/main/java/org/example/Types.java
+++ src/main/java/org/example/Types.java
@@ -1,63 +1,92 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Collections;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public enum Types {
-    // LEAFS
-    CRONOS_7023L("Cronos_7023"),
-    CRONOS_6021L("Cronos_6021"),
+import static org.example.Types.Hibernate.factory;
 
-    CRONOS_6001_A("Cronos 6001-A"),
-    CRONOS_6003("Cronos 6003"),
-    CRONOS_7023("Cronos 7023"),
+public class Types {
 
-    CRONOS_6021_NG("Cronos 6021-NG"),
-    CRONOS_6001_NG("Cronos 6001-NG"),
-    CRONOS_6003_NG("Cronos 6003-NG"),
-    CRONOS_6031_NG("Cronos 6031-NG"),
-    CRONOS_7021_NG("Cronos 7021-NG"),
-    CRONOS_7023_NG("Cronos 7023-NG"),
+    public static class Hibernate {
 
-    ARES_7023("Ares 7023"),
-    ARES_7031("Ares 7031"),
-    ARES_7021("Ares 7021"),
+        public static final SessionFactory factory =
+                new Configuration()
+                        .configure()
+                        .buildSessionFactory();
 
-    ARES_8023_15("Ares 8023 15"),
-    ARES_8023_200("Ares 8023 200"),
-    ARES_8023_2_5("Ares8023 2,5"),
-
-    // BRANCHES
-    CRONOS_L("Cronos L", CRONOS_7023L, CRONOS_6021L),
-    CRONOS_OLD("Cronos Old", CRONOS_6001_A, CRONOS_6003, CRONOS_7023),
-    CRONOS_NG("Cronos-NG", CRONOS_6001_NG, CRONOS_6003_NG, CRONOS_6021_NG, CRONOS_6031_NG, CRONOS_7021_NG, CRONOS_7023_NG),
-
-    ARES_TB("Ares TB", ARES_7023, ARES_7031, ARES_7021),
-    ARES_THS("Ares THS", ARES_8023_15, ARES_8023_200, ARES_8023_2_5),
-
-    // ROOTS
-    CRONOS("CRONOS", CRONOS_L, CRONOS_OLD, CRONOS_NG),
-    ARES("ARES", ARES_TB, ARES_THS);
-
-    private final String label;
-    private final List<Types> children;
-
-    Types(String label) {
-        this.label = label;
-        this.children = Collections.emptyList();
+        public static SessionFactory getSessionFactory() {
+            return factory;
+        }
     }
 
-    Types(String label, Types... children) {
-        this.label = label;
-        this.children = Arrays.asList(children);
+    public static void main(String[] args) {
+
+        SessionFactory factory = Hibernate.getSessionFactory();
+
+        try {
+        Session session = factory.openSession();
+            List<Line> lines = session
+                    .createQuery("FROM Line", Line.class)
+                    .getResultList();
+
+            for (Line line : lines) {
+                System.out.println("Line: " + line.getName());
+
+                for (Category cat : line.getCat()) {
+                    System.out.println("  Category: " + cat.getName());
+
+                    for (Model model : cat.getModel()) {
+                        System.out.println("    Model: " + model.getName());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            factory.close();
+        }
     }
 
-    public String getLabel() {
-        return label;
+    public static List<Line> getLines() {
+        List<Line> lines = new ArrayList<>();
+        try {
+            Session session = factory.openSession();
+            lines = session
+                    .createQuery("FROM Line", Line.class)
+                    .getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+    public static List<Category> getCategories() {
+        List<Category> cat = new ArrayList<>();
+        try {
+            Session session = factory.openSession();
+            cat = session
+                    .createQuery("FROM Category", Category.class)
+                    .getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cat;
     }
 
-    public List<Types> getChildren() {
-        return children;
+    public static List<Model> getModels() {
+        List<Model> models = new ArrayList<>();
+        try {
+            Session session = factory.openSession();
+            models = session
+                    .createQuery("FROM Model", Model.class)
+                    .getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return models;
     }
 }
+
