f21f7776dda5be722d25500a4fdc7c21e89b4135
João Felipe <joaofelipegaldinodelima75@gmail.com>
2026-04-16 14:12:10 -0300

    ##adding DTO's

---
+++ src/main/java/api/dto/LineDTO.java
@@ -0,0 +1,33 @@
+package api.dto;
+
+import java.util.List;
+
+public class LineDTO {
+
+    private int id;
+    private String name;
+    private List<CategoryDTO> cat;
+
+    public LineDTO(){
+
+    }
+
+    public LineDTO(int  id, String name,  List<CategoryDTO> cat) {
+        this.id = id;
+        this.name = name;
+        this.cat = cat;
+
+    }
+
+    public int getId(){return id; }
+
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public List<CategoryDTO> getCat() { return cat;}
+
+}
